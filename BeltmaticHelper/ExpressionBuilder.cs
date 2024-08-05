54223de55980678999bc9fc56ea2e5c76dc09e12
Loïse <izakhaak@gmail.com>
2024-08-05 18:55:09 +0200

    setup ExpressionBuilder

---
+++ BeltmaticHelper/ExpressionBuilder.cs
@@ -0,0 +1,32 @@
+﻿using BeltmaticHelper.DataTypes;
+
+namespace BeltmaticHelper;
+public class ExpressionBuilder
+{
+    public Expression Find(int numberToFind, int[] availableNumbers, Operator availableOperators)
+    {        
+        _availableOperators = availableOperators;
+        for (int i = availableNumbers.Length - 1; i >= 0; i--)
+        {
+            _expressionQueue.Enqueue(new Number
+            {
+                Value = availableNumbers[i],
+            });
+        }
+
+        while (true)
+        {
+            var item = _expressionQueue.Dequeue();
+
+            if (item.Result() == numberToFind)
+            {
+                return item;
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+
+    private Queue<Expression> _expressionQueue = new();
+    private Operator _availableOperators;
+}
