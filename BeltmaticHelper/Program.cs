54ac9e184d10c50a12576b817e45bc8710e77e66
Loïse <izakhaak@gmail.com>
2024-08-06 21:50:16 +0200

    basic user control

--- BeltmaticHelper/Program.cs
+++ BeltmaticHelper/Program.cs
@@ -8,11 +8,20 @@ internal class Program
     {
         Console.WriteLine("Hello, World!");
 
-        var expressionBuilder = new ExpressionBuilder(new int[] { 1, 2, 3, 4,5,6,8,7,9 }, new Operator[] { Operator.Adder, Operator.Multiplier });
+        var expressionBuilder = new ExpressionBuilder(
+            new int[] { 1, 2, 3, 4, 5, 6, 8, 7, 9, 11, 12 },
+            new Operator[] { Operator.Adder, Operator.Multiplier, Operator.Subtractor, Operator.Divider });
 
-        var result = expressionBuilder.Find(35805);
+        while (true)
+        {
+            while (int.TryParse(Console.ReadLine(), out var numberToFind))
+            {
+                var result = expressionBuilder.Find(numberToFind);
 
-        Console.WriteLine(result.ToString());
-        Console.ReadLine();
+                Console.WriteLine(result.ToString());
+
+            }
+            Console.WriteLine("That's not a number silly :)");
+        }
     }
 }
