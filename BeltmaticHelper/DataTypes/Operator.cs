f0ac292addaff292984d336a927406b939a776e0
Loïse <izakhaak@gmail.com>
2025-03-07 17:16:43 +0100

    #6 setup magic functions and clean up operator

--- BeltmaticHelper/DataTypes/Operator.cs
+++ BeltmaticHelper/DataTypes/Operator.cs
@@ -1,11 +1,10 @@
 ﻿namespace BeltmaticHelper.DataTypes;
 
-[Flags]
-public enum Operator : byte
+public enum Operator
 {
-    Adder = 1 << 0,
-    Multiplier = 1 << 1,
-    Subtractor = 1 << 2,
-    Divider = 1 << 3,
-    Exponentiator = 1 << 4,
+    Adder,
+    Multiplier,
+    Subtractor,
+    Divider,
+    Exponentiator,
 }
